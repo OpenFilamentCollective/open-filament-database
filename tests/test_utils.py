65b0004504b3a07eaf5d05bbe4c8e3c3f9caa8e5
Edvard Majakari <edvard@majakari.net>
2026-04-11 14:43:18 +0300

    format code

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,40 +11,52 @@ def test_generate_brand_uuid_matches_spec() -> None:
 
 def test_generate_material_uuid_matches_spec() -> None:
     brand_uuid = utils.generate_brand_uuid("Prusament")
-    assert utils.generate_material_uuid(brand_uuid, "PLA Prusa Galaxy Black") == "1aaca54a-431f-5601-adf5-85dd018f487f"
+    assert (
+        utils.generate_material_uuid(brand_uuid, "PLA Prusa Galaxy Black")
+        == "1aaca54a-431f-5601-adf5-85dd018f487f"
+    )
 
 
 def test_generate_material_uuid_accepts_uuid_object() -> None:
     brand_str = utils.generate_brand_uuid("Prusament")
     brand_obj = uuid.UUID(brand_str)
-    assert utils.generate_material_uuid(brand_str, "PLA") == utils.generate_material_uuid(brand_obj, "PLA")
+    assert utils.generate_material_uuid(brand_str, "PLA") == utils.generate_material_uuid(
+        brand_obj, "PLA"
+    )
 
 
-@pytest.mark.parametrize("text, expected", [
-    ("MixedCase",       "mixedcase"),
-    ("PLA Basic",       "pla_basic"),
-    ("pla-basic",       "pla_basic"),
-    ("Support@Plus!",   "supportplus"),
-    ("multiple   spaces", "multiple_spaces"),
-    ("_leading_",       "leading"),
-    ("PLA+",            "pla+"),
-    ("!!!",             ""),
-])
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("MixedCase", "mixedcase"),
+        ("PLA Basic", "pla_basic"),
+        ("pla-basic", "pla_basic"),
+        ("Support@Plus!", "supportplus"),
+        ("multiple   spaces", "multiple_spaces"),
+        ("_leading_", "leading"),
+        ("PLA+", "pla+"),
+        ("!!!", ""),
+    ],
+)
 def test_slugify(text: str, expected: str) -> None:
     assert utils.slugify(text) == expected
 
 
-@pytest.mark.parametrize("color, expected", [
-    ("#FF0000", "#FF0000"),
-    ("#ff0000", "#FF0000"),
-    ("#fff",    "#FFFFFF"),
-    ("fff",     "#FFFFFF"),
-    ("FF0000",  "#FF0000"),
-])
+@pytest.mark.parametrize(
+    "color, expected",
+    [
+        ("#FF0000", "#FF0000"),
+        ("#ff0000", "#FF0000"),
+        ("#fff", "#FFFFFF"),
+        ("fff", "#FFFFFF"),
+        ("FF0000", "#FF0000"),
+    ],
+)
 def test_normalize_color_hex(color: str, expected: str) -> None:
     assert utils.normalize_color_hex(color) == expected
 
 
+# This seems a bit odd. I'd expect code to throw in case like this, but implemented based on current behaviour.
 def test_normalize_color_hex_list_takes_first() -> None:
     assert utils.normalize_color_hex(["#ff0000", "#00ff00"]) == "#FF0000"
 
@@ -58,12 +70,15 @@ def test_normalize_color_hex_unparseable_returned_as_is() -> None:
     assert utils.normalize_color_hex("not-a-color") == "not-a-color"
 
 
-@pytest.mark.parametrize("value, expected", [
-    (None,       []),
-    ("pla",      ["pla"]),
-    (42,         [42]),
-    ({"k": "v"}, [{"k": "v"}]),
-    (["a", "b"], ["a", "b"]),
-])
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (None, []),
+        ("pla", ["pla"]),
+        (42, [42]),
+        ({"k": "v"}, [{"k": "v"}]),
+        (["a", "b"], ["a", "b"]),
+    ],
+)
 def test_ensure_list(value: object, expected: list) -> None:
     assert utils.ensure_list(value) == expected
